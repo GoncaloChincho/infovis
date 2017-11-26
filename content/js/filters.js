cc0577cba34a8e7dd12f1cac008b5e2a837bd6bd
Gonçalo Lopes <goncalochincho@gmail.com>
2017-11-26 16:58:38 +0000

    filters progress

--- content/js/filters.js
+++ content/js/filters.js
@@ -1,5 +1,18 @@
+const DISTANCE_MAX = 77.82;
+const DISTANCE_MIN = 0.04;
+const DIAMETER_MAX = 495;
+const DIAMETER_MIN = 1.65;
+const VELOCITY_MAX = 38.6;
+const VELOCITY_MIN = 1;
+const IMPACT_PROBABILITY_MAX = 0.96;
+const IMPACT_PROBABILITY_MIN = 1.2e-9;
+const HAZARD_SCALE_MAX = -1.4;
+const HAZARD_SCALE_MIN = -11.4;
+let AVERAGE_NEO = null;
+let DATA = null;
 activeFilters = ['dist-top15'];
 styleActiveClass = 'nice-button-active';
+const svg = d3.select("#svgId");
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
@@ -36,8 +49,24 @@ function removeOtherFilters(currFilter){
     
 }
 
+function fileFromFilters(){
+    activeFilters.sort();
+    var file ="content/data/";
+    if(activeFilters.length == 0){
+        return file += 'neos.json';
+    }
+    var folder = activeFilters[0].split('-')[0]; + "/";
+    file += folder + "/";
+    for(i = 0; i < activeFilters.length;i++){
+        file += activeFilters[i] + '&';
+    }
+    file = file.slice(0,-1);
+    return (file + '.json');
+}
+
 $(document).ready(function(){
     activateFilterBtns();
+    loadCharts("content/data/dmg/dmg-1.json");
     $(".nice-button").click(function(){
        var item = $(this);
        var filter = item.val();
@@ -52,7 +81,44 @@ $(document).ready(function(){
            activeFilters.push(filter);
            //add filter
        }
+       loadCharts(fileFromFilters(),true);
        activateFilterBtns();
-       console.log(activeFilters);
+       
     });
 });
+
+
+ 
+function loadCharts(data,update=false){
+    if(update){
+        svg.selectAll("*").remove();
+    }
+    d3.json(data, function(error, graph) {
+    if (error) throw error;
+    DATA = graph;
+    let distanceSum = 0;
+    let diameterSum = 0;
+    let velocitySum = 0;
+    let impactProbabilitySum = 0;
+    let hazardScaleSum = 0;
+
+    for( var i = 0; i < graph.length; i++ ){
+        distanceSum += graph[i].distance;
+        diameterSum += graph[i].diameter;
+        velocitySum += graph[i].velocity;
+        impactProbabilitySum += graph[i].impactProbability;
+        hazardScaleSum += graph[i].hazardScale;
+    }
+    AVERAGE_NEO = {
+        name: 'Average NEO',
+        distance: distanceSum/graph.length,
+        diameter: diameterSum/graph.length,
+        velocity: velocitySum/graph.length,
+        impactProbability: impactProbabilitySum/graph.length,
+        hazardScale: hazardScaleSum/graph.length,
+    }
+    initOrbitalLayout();
+    initRadarChart();
+ });
+}
+ 
