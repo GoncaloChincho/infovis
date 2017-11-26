fd661cab2908122e43ba323793a251be7dec0d87
Gonçalo Lopes <goncalochincho@gmail.com>
2017-11-26 17:47:09 +0000

    progress

--- components/orbitalLayout.js
+++ components/orbitalLayout.js
@@ -1,33 +1,34 @@
-var getBaseLog = (x, y) => {
-	  return Math.log(y) / Math.log(x);
-}
-var getR = (diameter) => {
-	if(diameter < 50) return 3;
-	if(diameter < 150) return 6;
-	if(diameter < 250) return 9;
-	if(diameter < 350) return 10;
-	if(diameter < 450) return 11;
-	if(diameter < 550) return 12;
-	else return 15;
-}
-
 var getC = (impactDamage) => {
-	if(impactDamage === 0) return "green";
-	if(impactDamage === 1) return "yellow";
-	if(impactDamage === 2) return "orange";
-	if(impactDamage === 3) return "red";
-	else return "blue";
-}
+        if(impactDamage === 0) return "green";
+        if(impactDamage === 1) return "yellow";
+        if(impactDamage === 2) return "orange";
+        if(impactDamage === 3) return "red";
+        else return "blue";
+    }
 
-var simulation = d3.forceSimulation()
+var initOrbitalLayout = () => {
+    var getBaseLog = (x, y) => {
+	  return Math.log(y) / Math.log(x);
+    }
+    var getR = (diameter) => {
+        if(diameter < 50) return 3;
+        if(diameter < 150) return 6;
+        if(diameter < 250) return 9;
+        if(diameter < 350) return 10;
+        if(diameter < 450) return 11;
+        if(diameter < 550) return 12;
+        else return 15;
+    }
+
+    
+    var simulation = d3.forceSimulation()
 	.force("link", d3.forceLink().id((d) => d.objectId).distance((d) => getBaseLog(1.0001, d.distance + 10) / 165 - 130))
 	.force("collide", d3.forceCollide().radius((d) => getR(d.diameter)))
 	.force("charge", d3.forceManyBody().strength(-30))
 	.alphaDecay(0.05);
+    
 	// .force("center", d3.forceCenter());
 	// .force("r", d3.forceRadial().strength(0.01));
-	
-var initOrbitalLayout = () => {
 	var links = DATA.map(o => { return {source: 'Earth', target: o.objectId, distance: o.distance}});
 	var nodes = [{objectId: 'Earth', fx: 0, fy: 0}, ...DATA];
 	var node = svg.append("g")
