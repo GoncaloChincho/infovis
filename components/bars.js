07b3cd7cb3284a55229a1eafb754bd2c06f58054
jessicaar <jessica_a.r@hotmail.com>
2017-12-15 22:04:48 +0000

    bars.js

--- components/bars.js
+++ components/bars.js
@@ -6,27 +6,66 @@ d3.json("content/data/occurences.json", function (data){
 })
 
 function gen_vis(){
-	var w = 680;
-	var h = 230;
 	
-	yscale = d3.scaleLog()
-				.domain([0.5, h])
-				.range([0, h]);
-		
+	var window_height = window.innerHeight;
+    var window_width = window.innerWidth;
+    h = 0.35 * window_height;
+    w = 0.7 * window_width;
+	var bar_w =	Math.floor(w/60)-1;	
+	
+	var	yscale = d3.scaleLog()
+						.domain([0.5,100])	
+						.range([0,h-50]);	
+	
+	var	xscale = d3.scaleLinear()	
+						.domain([0,61])	
+						.range([0,w]);	
+	
+	var yaxis = d3.axisRight()
+				.scale(yscale)
+				.tickFormat(function(d) {return yscale.tickFormat(4,d3.format(",d"))(d)})
+
+	var x0 = dataset[0].Year;
+	var xn = dataset[60].Year;
+	
+	var xaxis =	d3.axisBottom()
+						.scale(d3.scaleLinear()	
+							.domain([x0,xn])	
+							.range([bar_w/2,w-bar_w/2]))	
+						.tickFormat(function(d){if(d!==x0 && d!==xn){return d}})	
+						.ticks(60/10);
+	
+	var xaxis2 = d3.axisTop()	
+						.scale(d3.scaleLinear()	
+							.domain([dataset[0].Year,dataset[60].Year])	
+							.range([bar_w/2,w-bar_w/2]))	
+						.tickFormat("")	
+						.ticks(60/10);
+	
 	var svg = d3.select("#svgBars")
 				.append("g")
 				.attr("width", w)
 				.attr("height", h);
-	console.log(dataset);
-
+				
 	svg.selectAll("rect")	
 		.data(dataset)
 		.enter().append("rect")	
-			.attr("width", (w/269))	
-			.attr("height", function(d)	{return	yscale(d.Occurences*2);})	
+			.attr("width", bar_w)	
+			.attr("height", function(d)	{return	yscale(d.Occurences);})	
 			.attr("fill","purple")
-			.attr("x",function(d,i)	{return	i*3.5;})
-			.select("title")
-				.text(function(d){return d.Year});
-	
+			.attr("x",function(d,i){return xscale(i);})
+			.attr("y",30)
+			.append("title")
+				.text(function(d){return d.Year});				
+					
+	svg.append("g")
+		.attr("transform", "translate(0,30)")
+		.call(yaxis);
+		
+	svg.append("g")
+			.call(xaxis);
+			
+	svg.append("g")	
+			.attr("transform","translate(0,30)")
+			.call(xaxis2);
 }
